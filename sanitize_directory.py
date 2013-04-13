081c8bebb4000dc5a50e5c4ebe77140c3f683326
Eric Petersen <eric@huskers.unl.edu>
2013-04-12 23:14:13 -0500

    added more test dirs and directory renaming

--- sanitize_directory.py
+++ sanitize_directory.py
@@ -26,19 +26,34 @@ def main():
         '--match', metavar='match', type=str,
         help='optional regex against which to match filenames'
     )
+    parser.add_argument(
+        '-v', dest='verbose', action='store_true',
+        help='more descriptive output'
+    )
     args = parser.parse_args()
 
+    # check filesystem
     if not args.filesystem in sanitize.FILE_SYSTEMS:
         sys.stdout.write('Invalid file system.\n')
         return 1
 
     for os_tup in os.walk(args.directory):
         (os_dir, os_dirs, os_files) = os_tup
-        for os_file in os_files:
-            sanitized = sanitize.sanitize_filename(os_file, args.filesystem)
+        in_dir = os.path.basename(os_dir)
+        san_dir = sanitize.sanitize_filename(in_dir, args.filesystem)
+        out_dir = os.path.join(os.path.dirname(os_dir), san_dir)
+        if args.verbose:
             sys.stdout.write('+--------------------------------------\n')
-            sys.stdout.write('| original:  {0}\n'.format(os_file))
-            sys.stdout.write('| sanitized: {0}\n'.format(sanitized))
+            sys.stdout.write('| original:  {0}\n'.format(os_dir))
+            sys.stdout.write('| sanitized: {0}\n'.format(out_dir))
+        for os_file in os_files:
+            in_file = os.path.join(out_dir, os_file)
+            sanitized = sanitize.sanitize_filename(os_file, args.filesystem)
+            out_file = os.path.join(out_dir, sanitized)
+            if args.verbose:
+                sys.stdout.write('+--------------------------------------\n')
+                sys.stdout.write('| original:  {0}\n'.format(in_file))
+                sys.stdout.write('| sanitized: {0}\n'.format(out_file))
     sys.stdout.write('+--------------------------------------\n')
 
 if __name__ == '__main__':
