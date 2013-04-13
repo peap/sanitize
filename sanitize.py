fb0477654877178ec6b74d62bce84fc438ad561d
Eric Petersen <eric@huskers.unl.edu>
2013-04-12 21:38:49 -0500

    broke up some long lines and added some missing colons

--- sanitize.py
+++ sanitize.py
@@ -1,12 +1,26 @@
 import unicodedata
 
-def sanitize_path_fragment(
-        original_fragment,
-        target_file_systems = {'btrfs', 'ext', 'ext2', 'ext3', 'ext3cow', 'ext4', 'exfat', 'fat32', 'hfs+', 'ntfs_win32', 'reiser4', 'reiserfs', 'xfs', 'zfs'},
-        sanitization_method = 'underscore',
-        truncate = True
-    ):
+def sanitize_path_fragment(original_fragment,
+                           target_file_systems = {
+                               'btrfs',
+                               'ext',
+                               'ext2',
+                               'ext3',
+                               'ext3cow',
+                               'ext4',
+                               'exfat',
+                               'fat32',
+                               'hfs+',
+                               'ntfs_win32',
+                               'reiser4',
+                               'reiserfs',
+                               'xfs',
+                               'zfs'},
+                               sanitization_method = 'underscore',
+                               truncate = True):
+
     sanitized_fragment = unicodedata.normalize('NFC', original_fragment)
+
     if sanitization_method == 'underscore':
         illegal_characters = {
             'btrfs': {'\0', '/'},
@@ -41,9 +55,17 @@ def sanitize_path_fragment(
         # Truncate if the resulting string is too long
         if truncate:
             max_lengths = {
-                # For the entries of file systems commonly found with Linux, the length, 'utf-8', and 'NFC' are only assumptions that apply to mostly vanilla kernels with default build parameters.
-                # Seriously, this is 2013. The fact that the Linux community does not move to a file system with an enforced Unicode filename encoding is as bad as Windows 95's codepage madness, some 18 years ago.
-                # If you add more file systems, see if it is affected by Unicode Normal Forms, like HFS+; You may have to take extra care in editing the actual sanitization routine below.
+                #   For the entries of file systems commonly found with Linux,
+                # the length, 'utf-8', and 'NFC' are only assumptions that
+                # apply to mostly vanilla kernels with default build
+                # parameters.
+                #   Seriously, this is 2013. The fact that the Linux community
+                # does not move to a file system with an enforced Unicode
+                # filename encoding is as bad as Windows 95's codepage madness,
+                # some 18 years ago.
+                #   If you add more file systems, see if it is affected by
+                # Unicode Normal Forms, like HFS+; You may have to take extra
+                # care in editing the actual sanitization routine below.
                 'btrfs': (255, 'bytes', 'utf-8', 'NFC'),
                 'ext': (255, 'bytes', 'utf-8', 'NFC'),
                 'ext2': (255, 'bytes', 'utf-8', 'NFC'),
@@ -51,11 +73,19 @@ def sanitize_path_fragment(
                 'ext3cow': (255, 'bytes', 'utf-8', 'NFC'),
                 'ext4': (255, 'bytes', 'utf-8', 'NFC'),
                 'exfat': (255, 'characters', 'utf-16', 'NFC'),
-                'fat32': (255, 'characters', 'utf-16', 'NFC'), # 'utf-16' is not entirely true. FAT32 used to be used with codepages; but since Windows XP, the default seems to be UTF-16.
-                'hfs+': (255, 'characters', 'utf-16', 'NFD'), # FIXME: improve HFS+ handling, because it does not use the standard NFD. It's close, but it's not exactly the same thing.
+                # 'utf-16' is not entirely true. FAT32 used to be used with
+                # codepages; but since Windows XP, the default seems to be
+                # UTF-16.
+                'fat32': (255, 'characters', 'utf-16', 'NFC'),
+                # FIXME: improve HFS+ handling, because it does not use the
+                # standard NFD. It's close, but it's not exactly the same
+                # thing.
+                'hfs+': (255, 'characters', 'utf-16', 'NFD'),
                 'ntfs_win32': (255, 'characters', 'utf-16', 'NFC'),
                 'ntfs_posix': (255, 'characters', 'utf-16', 'NFC'),
-                'reiser4': (3976, 'bytes', 'utf-8', 'NFC'), # I don't care if Linux can't support >255 bytes. The adoption of filenames longer than 255 bytes is long overdue.
+                # I don't care if Linux can't support >255 bytes. The adoption
+                # of filenames longer than 255 bytes is long overdue.
+                'reiser4': (3976, 'bytes', 'utf-8', 'NFC'),
                 'reiserfs': (4032, 'bytes', 'utf-8', 'NFC'), # Same here.
                 'xfs': (255, 'bytes', 'utf-8', 'NFC'),
                 'zfs': (255, 'bytes', 'utf-8', 'NFC'),
@@ -72,9 +102,9 @@ def sanitize_path_fragment(
                     sanitized_fragment = unicodedata.normalize('NFC', temp_fragment_bytes.decode(max_lengths[file_system][2]))
                 else: # Assume 'characters'
                     temp_fragment = ''
-                    if file_system == 'hfs+'
+                    if file_system == 'hfs+':
                         normalize = unicodedata.ucd_3_2_0.normalize
-                    else
+                    else:
                         normalize = unicodedata.normalize
                     for character in sanitized_fragment:
                         normalized_character = normalize(max_lengths[file_system][3], character)
